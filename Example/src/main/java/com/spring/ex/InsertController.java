5fca2621131d7e1e06ae2d47c8a591bd626c7e87
yuwls <kangchull59@gmail.com>
2023-12-05 14:09:10 +0900

    second 14:08

---
+++ Example/src/main/java/com/spring/ex/InsertController.java
@@ -0,0 +1,5 @@
+package com.spring.ex;
+
+public class InsertController {
+	//second에서 작성
+}
--- Example/src/main/java/com/spring/ex/ListController.java
+++ Example/src/main/java/com/spring/ex/ListController.java
@@ -5,5 +5,7 @@ public class ListController {
 	//master Controller	또 수정
 	//master Controller
 	//master Controller 추가함
+	//안녕
+	
 
 }
